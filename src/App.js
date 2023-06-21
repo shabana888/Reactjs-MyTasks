5f1d014eeeef2326586c0e778136e26302aacf59
shabana888 <shaikshabana.btc@gmail.com>
2023-06-21 14:54:18 +0000

    my-tasks

---
+++ src/App.js
@@ -0,0 +1,37 @@
+// These are the lists used in the application. You can move them to any component needed.
+
+/* const tagsList = [
+  {
+    optionId: 'HEALTH',
+    displayText: 'Health',
+  },
+  {
+    optionId: 'EDUCATION',
+    displayText: 'Education',
+  },
+  {
+    optionId: 'ENTERTAINMENT',
+    displayText: 'Entertainment',
+  },
+  {
+    optionId: 'SPORTS',
+    displayText: 'Sports',
+  },
+  {
+    optionId: 'TRAVEL',
+    displayText: 'Travel',
+  },
+  {
+    optionId: 'OTHERS',
+    displayText: 'Others',
+  },
+]
+*/
+
+import Home from './components/Home'
+
+import './App.css'
+
+const App = () => <Home />
+
+export default App
